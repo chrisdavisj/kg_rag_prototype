4499adb76cf5fb568af7c3122cf2b2e318c41fc5
Chris Davis J <jaldi.2@wright.edu>
2025-06-27 12:26:30 -0400

    added multithreading for faster processing

--- sparql/hops.py
+++ sparql/hops.py
@@ -1,32 +1,46 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from config import get, put
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 SPARQL_ENDPOINT = get("sparql.endpoint")
 PREFERRED_CONTEXT_HOPS = get("thresholds.preferred_context_hops")
+MAX_WORKERS_FOR_THREAD_POOL = get("multi_threading.num_workers_to_be_used")
+
+
+def query_hops(class1, class2):
+    sparql = SPARQLWrapper(SPARQL_ENDPOINT)
+    query = f"""
+    SELECT (COUNT(?mid) AS ?hops) WHERE {{
+      <{class1}> ?p1 ?mid .
+      ?mid ?p2 <{class2}> .
+    }} LIMIT 1
+    """
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    try:
+        results = sparql.query().convert()
+        value = results['results']['bindings'][0].get('hops', {}).get('value')
+        if value:
+            return int(value)
+    except:
+        return None
 
 
 def find_min_hops_sparql(classes):
-    sparql = SPARQLWrapper(SPARQL_ENDPOINT)
-    min_hops = float('inf')
-    for i, class1 in enumerate(classes):
-        for class2 in classes[i+1:]:
-            query = f"""
-            SELECT (COUNT(?mid) AS ?hops) WHERE {{
-              <{class1}> (<>|!<>)* ?mid .
-              ?mid (<>|!<>)* <{class2}> .
-            }} LIMIT 1
-            """
-            sparql.setQuery(query)
-            sparql.setReturnFormat(JSON)
-            try:
-                results = sparql.query().convert()
-                value = results['results']['bindings'][0].get(
-                    'hops', {}).get('value')
-                if value:
-                    min_hops = max(min_hops, int(value))
-            except:
-                continue
-    preferred_hops = min_hops if min_hops != float(
+    min_hops_required = float('inf')
+    futures = []
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS_FOR_THREAD_POOL) as executor:
+        for i, class1 in enumerate(classes):
+            for class2 in classes[i+1:]:
+                futures.append(executor.submit(query_hops, class1, class2))
+
+        for future in as_completed(futures):
+            result = future.result()
+            if result is not None:
+                min_hops_required = max(min_hops_required, result)
+
+    preferred_hops = min_hops_required if min_hops_required != float(
         'inf') else PREFERRED_CONTEXT_HOPS
     put("thresholds.preferred_context_hops", preferred_hops)
     return preferred_hops
